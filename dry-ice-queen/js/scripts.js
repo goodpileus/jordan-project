288457e3847a26565b92e3accd5d4f2a040c683c
Aidan Quinlan <aidankquinlan@gmail.com>
2023-10-22 18:45:14 -0700

    dry ice queen

---
+++ dry-ice-queen/js/scripts.js
@@ -0,0 +1,90 @@
+// init controller
+var controller = new ScrollMagic.Controller({globalSceneOptions: {
+	// duration: '100%'
+}});
+
+// s1
+new ScrollMagic.Scene({triggerElement: "#drift"})
+				.setClassToggle(".drift-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#hd"})
+				.setClassToggle(".hd-bg", "active") 
+				// .addIndicators() 
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#eyes"})
+				.setClassToggle(".eyes-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#loss"})
+				.setClassToggle(".loss-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#failure"})
+				.setClassToggle(".failure-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#goosepimples"})
+				.setClassToggle(".goosepimples-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#labyrinth"})
+				.setClassToggle(".labyrinth-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#voice"})
+				.setClassToggle(".voice-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#sunshine"})
+				.setClassToggle(".sunshine-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#night"})
+				.setClassToggle(".night-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#door"})
+				.setClassToggle(".door-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#corridor"})
+				.setClassToggle(".corridor-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#vulva"})
+				.setClassToggle(".vulva-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#awake"})
+				.setClassToggle(".awake-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#fade"})
+				.setClassToggle(".fade-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#blank"})
+				.setClassToggle(".blank-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#end"})
+				.setClassToggle(".end-bg", "active")
+				// .addIndicators()
+				.addTo(controller);
